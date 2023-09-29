ef9850765c83769588502a16dcb212c5b6f1242d
Nick VanCise <nickvancise@MacBook-Pro.local>
2023-09-28 17:50:48 -0700

    feat: tab navigation in modals
    - give react modal portal a height so modals can use tab navigation
    
    - hoist enabling/disabling emulator keyboard input

--- beta/gbajs3/src/components/modals/modal-container.tsx
+++ beta/gbajs3/src/components/modals/modal-container.tsx
@@ -1,12 +1,13 @@
 import { useContext } from 'react';
 import Modal from 'react-modal';
 
+import { EmulatorContext } from '../../context/emulator/emulator.tsx';
 import { ModalContext } from '../../context/modal/modal.tsx';
 
 const modalStyles = {
   overlay: {
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    zIndex: 100,
+    zIndex: 100
   },
   content: {
     width: 'calc(100dvw - 20px)',
@@ -18,13 +19,14 @@ const modalStyles = {
     padding: '0',
     maxHeight: '90dvh',
     display: 'flex',
-    flexDirection: 'column' as const,
-  },
+    flexDirection: 'column' as const
+  }
 };
 
 export const ModalContainer = () => {
   const { modalContent, isModalOpen, setIsModalOpen } =
     useContext(ModalContext);
+  const { emulator } = useContext(EmulatorContext);
 
   return (
     <Modal
@@ -33,6 +35,8 @@ export const ModalContainer = () => {
       isOpen={isModalOpen}
       style={modalStyles}
       onRequestClose={() => setIsModalOpen(false)}
+      onAfterOpen={emulator?.disableKeyboardInput}
+      onAfterClose={emulator?.enableKeyboardInput}
     >
       {modalContent}
     </Modal>
