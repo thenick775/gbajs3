5fb72779d81dd031d132150bb1e9a47bdbd5b971
thenick775 <nvancisedev@gmail.com>
2024-04-29 18:14:35 -0700

    refactor: useid as prefix where necessary
    - fixes an antipattern laid out in: https://hetdesai03.medium.com/a-complete-guide-to-useid-hook-in-react-18-22119ecfd87f

--- gbajs3/src/components/modals/save-states.tsx
+++ gbajs3/src/components/modals/save-states.tsx
@@ -99,9 +99,7 @@ export const SaveStatesModal = () => {
     saveStateSlotLocalStorageKey,
     0
   );
-  const saveStatesFormId = useId();
-  const saveStatesListId = useId();
-  const addStateButtonId = useId();
+  const baseId = useId();
   const {
     register,
     handleSubmit,
@@ -142,7 +140,7 @@ export const SaveStatesModal = () => {
         </p>
       ),
       placementBeacon: 'bottom-end',
-      target: `#${CSS.escape(saveStatesFormId)}`
+      target: `#${CSS.escape(`${baseId}--save-state-slot-form`)}`
     },
     {
       content: (
@@ -152,7 +150,7 @@ export const SaveStatesModal = () => {
         </p>
       ),
       placementBeacon: 'bottom-end',
-      target: `#${CSS.escape(saveStatesListId)}`
+      target: `#${CSS.escape(`${baseId}--save-state-list`)}`
     },
     {
       content: (
@@ -162,7 +160,7 @@ export const SaveStatesModal = () => {
         </p>
       ),
       placementBeacon: 'bottom-end',
-      target: `#${CSS.escape(addStateButtonId)}`
+      target: `#${CSS.escape(`${baseId}--add-state-button`)}`
     }
   ];
 
@@ -170,7 +168,10 @@ export const SaveStatesModal = () => {
     <>
       <ModalHeader title="Manage Save States" />
       <ModalBody>
-        <StyledForm id={saveStatesFormId} onSubmit={handleSubmit(onSubmit)}>
+        <StyledForm
+          id={`${baseId}--save-state-slot-form`}
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <TextField
             label="Current Save State Slot"
             type="number"
@@ -187,7 +188,7 @@ export const SaveStatesModal = () => {
             })}
           />
           <Button
-            form={saveStatesFormId}
+            form={`${baseId}--save-state-slot-form`}
             type="submit"
             variant="outlined"
             size="small"
@@ -196,7 +197,7 @@ export const SaveStatesModal = () => {
           </Button>
         </StyledForm>
 
-        <SaveStatesList id={saveStatesListId}>
+        <SaveStatesList id={`${baseId}--save-state-list`}>
           {renderedSaveStates?.map?.((saveState: string, idx: number) => (
             <StyledLi key={`${saveState}_${idx}`}>
               <LoadSaveStateButton
@@ -241,7 +242,7 @@ export const SaveStatesModal = () => {
           )}
         </SaveStatesList>
         <IconButton
-          id={addStateButtonId}
+          id={`${baseId}--add-state-button`}
           aria-label={`Create new save state`}
           sx={{ padding: 0 }}
           onClick={() => {
