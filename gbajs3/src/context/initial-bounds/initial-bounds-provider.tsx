74fc37648428e0290d23722751dd93fdb8e6d3fe
thenick775 <nvancisedev@gmail.com>
2025-09-24 20:53:12 -0700

    fix: initial bounds provider+screen tests

--- gbajs3/src/context/initial-bounds/initial-bounds-provider.tsx
+++ gbajs3/src/context/initial-bounds/initial-bounds-provider.tsx
@@ -1,5 +1,11 @@
 import { useOrientation, useWindowSize } from '@uidotdev/usehooks';
-import { useCallback, useEffect, useState, type ReactNode } from 'react';
+import {
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+  type ReactNode
+} from 'react';
 
 import { InitialBoundsContext } from './initial-bounds-context.tsx';
 
@@ -13,6 +19,10 @@ export const InitialBoundsProvider = ({
   const [initialBounds, setInitialBounds] = useState<InitialBounds>();
   const orientation = useOrientation();
   const windowSize = useWindowSize();
+  const prevSize = useRef<{
+    width: number | null;
+    height: number | null;
+  }>(windowSize);
 
   const setInitialBound = useCallback(
     (key: string, bounds?: DOMRect) =>
@@ -28,17 +38,22 @@ export const InitialBoundsProvider = ({
   const hasInitialBounds = !!initialBounds;
 
   useEffect(() => {
-    if (
-      orientation.angle !== null &&
-      [0, 90, 270].includes(orientation.angle) &&
-      hasInitialBounds
-    )
+    if ([0, 90, 270].includes(orientation.angle) && hasInitialBounds)
       clearInitialBounds();
   }, [clearInitialBounds, orientation.angle, hasInitialBounds]);
 
   useEffect(() => {
-    if (windowSize.width && windowSize.height && hasInitialBounds)
+    if (
+      windowSize.width !== prevSize.current.width &&
+      windowSize.height !== prevSize.current.height &&
+      hasInitialBounds
+    ) {
       clearInitialBounds();
+      prevSize.current = {
+        width: windowSize.width,
+        height: windowSize.height
+      };
+    }
   }, [
     clearInitialBounds,
     windowSize.width,
